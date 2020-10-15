382ade1965939e200c3215bad168bfd3865ccfa1
Aleksandr <foxi4554@gmail.com>
2020-10-15 15:19:50 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str == undefined || str == '' || str.length % 2 != 0) return false;
+
+    const bracketPairs = [];
+    bracketsConfig.forEach(element => {
+        bracketPairs.push(element.join('')); 
+    });
+
+    let strLength = str.length;
+    while (true) {
+        for (let i = 0; i < bracketPairs.length; i++) {
+            str = str.replace(bracketPairs[i], '');
+        }
+    
+        if (str.length == 0) { return true;
+        } else if (strLength == str.length) return false;
+    
+        strLength = str.length;
+    }
 }
